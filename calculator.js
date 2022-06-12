9b9e8366635715b39b6a250d0d8326a3a0189d58
Avery Yang <averyyang2674@gmail.com>
2022-06-12 16:29:55 -0500

    app.get and app.listen

--- calculator.js
+++ calculator.js
@@ -1,3 +1,11 @@
 //require express module
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.listen(3000, function () {
+    console.log('Your server has started on port 3000!');
+});
